f45c13d71015f34944dc9255f402ba4685bb5fc6
alexfiftysix <obscure1>
2019-04-10 17:46:18 +1000

    Gradients are now more nice.

--- app.py
+++ app.py
@@ -3,55 +3,71 @@ import random
 
 from affirmation_generator import generate_affirmation
 
+# TODO: Allow to go back to previous affirmations
+
 app = Flask(__name__)
 
-COLOURS = [
-    (0, 255, 255),  # CYAN
-    (0, 255, 0),  # GREEN
-    (255, 0, 200),  # PINK
-    # (255, 255, 0),  # YELLOW
-    # (255, 165, 0),  # ORANGE
-    (200, 0, 255),  # PURPLE
-    (0, 180, 255),  # BLUE
-    (255, 0, 102),  # HOT PINK
-    (127, 255, 36),  # ALERT
-    (205, 233, 202),  # BRIGHT EYES
-    (255, 72, 71),  # CORAL RED
-    (181, 255, 206),  # FRESHMINT
-    (252, 6, 67),  # ELECTRIC CRIMSON
-    (255, 117, 255),  # SOFT PINK
-]
 
-GRADIENT_DIRECTIONS = [
-    'to bottom right',
-    'to bottom left',
-    'to top right',
-    'to top left',
-    'to bottom',
-    'to right',
-    'to top',
-    'to left'
-]
-
-
+# TODO: Sentiment analysis of sentences to decide on appropriate colours.
 # TODO: different fonts
 # TODO: Mobile support
 
+
+def generate_gradient_direction():
+    """
+    returns a randomly chosen gradient direction from those allowed in css
+    """
+    potential_directions = [
+        'to bottom right',
+        'to bottom left',
+        'to top right',
+        'to top left',
+        'to bottom',
+        'to right',
+        'to top',
+        'to left'
+    ]
+
+    return random.choice(potential_directions)
+
+
+def generate_gradient():
+    """
+    returns a gradient which can be dropped into css on an element and will function as a 'background-image'
+    Chooses bright colours which should still support white text
+    """
+    # Yellow-brown range colours can look ugly
+    hue_1 = hue_2 = hue_3 = 35
+    while 30 <= hue_1 <= 80:
+        hue_1 = random.randint(0, 359)
+    saturation_1 = 100
+    lightness_1 = 50
+
+    gradient_step = random.randint(15, 45)
+
+    # To avoid yellow-browns
+    if hue_1 <= 30:
+        hue_2 = hue_1 - gradient_step
+        hue_3 = hue_2 - gradient_step
+    else:
+        hue_2 = hue_1 + gradient_step
+        hue_3 = hue_2 + gradient_step
+
+    color_1 = f"hsl({hue_1}, {saturation_1}%, {lightness_1}%)"
+    color_2 = f"hsl({hue_2}, {saturation_1}%, {lightness_1}%)"
+    color_3 = f"hsl({hue_3}, {saturation_1}%, {lightness_1}%)"
+
+    gradient_direction = generate_gradient_direction()
+
+    return f"background-image: linear-gradient({gradient_direction}, {color_1}, {color_2}, {color_3})"
+
+
 @app.route('/')
 def hello_world():
+    color = generate_gradient()
     message = generate_affirmation()
 
-    tc = COLOURS[random.randint(0, len(COLOURS) - 1)]
-    top_colour = '{}, {}, {}'.format(tc[0], tc[1], tc[2])
-
-    bc = [min(255, x + 150) for x in tc]
-    bottom_colour = '{}, {}, {}'.format(bc[0], bc[1], bc[2])
-
-    gradient_direction = GRADIENT_DIRECTIONS[random.randint(0, len(GRADIENT_DIRECTIONS) - 1)]
-
-    return render_template('affirmation.html', message=message, top_colour=top_colour, bottom_colour=bottom_colour,
-                           gradient_direction=gradient_direction)
-    # return 'Hello World!'
+    return render_template('affirmation.html', color=color, message=message)
 
 
 if __name__ == '__main__':
